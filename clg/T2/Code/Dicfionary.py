fa6923fe142c9552ed6e899ec695c247973d1216
mihir021 <rathodmihir1113@gmail.com>
2025-11-05 13:07:58 +0530

    d1

--- clg/T2/Code/Dicfionary.py
+++ clg/T2/Code/Dicfionary.py
@@ -50,17 +50,152 @@ print(d.keys())
 for i in d.keys():
     print(i)
 
-print("--------------------- keys ------------------------")
+print("--------------------- values ------------------------")
 
 print(d.values())
 for i in d.values():
     print(i)
 
-print("--------------------- keys ------------------------")
+print("--------------------- items ------------------------")
 
 print(d.items())
 for i in d.items():
     print(i)
+
+print("--------------------- items ------------------------")
+
+car = {'brand ':'Fode','mode':'Mustang','year':1964 }
+x = car.get('year')
+print(x)
+
+x = car.get('prize')
+print(x)
+print(car)
+
+x = car.get('prize',15000)
+print(x)
+print(car)
+
+x = car.get('year',1985)
+print(x)
+print(car)
+
+print("--------------------- setdefult() ------------------------")
+
+x = car.setdefault('year',9090)
+print(x)
+print(car)
+
+x = car.setdefault('color','aq')
+print(x)
+print(car)
+
+print("--------------------- Update() ------------------------")
+
+d = {1:'aa',2:'py',3:'asx'}
+x =  {4:'aza',5:'drftgyh'}
+
+print(d)
+print(x)
+
+car.update({'color':'whight'})
+print(car)
+
+print("--------------------- set() ------------------------")
+
+s = {"app","ban","che"}
+print(s)
+print(type(s))
+print()
+t = {1,5,7,8,3}
+t2 = {True,False,True}
+# var = t2[0] TypeError: 'set' object is not subscriptable
+t3 = {"abc",34,True,1,0,False}
+print(t)
+print(t2)
+print(t3)
+print(type(t))
+print(type(t2))
+print(type(t3))
+print()
+l = [1,2,3,4]
+s = set(l)
+print(s)
+print(type(s))
+print()
+a = {}
+print(type(a))
+a = set()
+print(type(a))
+
+t = {12,3,3}
+t.add(2)
+print(t)
+
+print("---------------------- union() ----------------------")
+x = {'a','b','c'}
+x1 = {'d','e','f'}
+x2 = {'d','e','c'}
+
+r = x|x1|x2
+print(r)
+
+r = x.union(x1,x2)
+print(r)
+print(type(r))
+print()
+
+print("---------------------- intersection() ----------------------")
+x = {'a','b','c'}
+x1 = {'d','e','f'}
+x2 = {'d','e','c'}
+
+r = x.intersection(x1,x2)
+print(r)
+print(type(r))
+print()
+
+print("---------------------- diff() ( - ) ----------------------")
+x = {'a','b','c'}
+x1 = {'g','m','a'}
+r = x1.difference(x)
+print(r)
+print(type(r))
+print()
+
+print("---------------------- symmetric() ----------------------")
+x = {'a','b','c'}
+x1 = {'g','m','a'}
+r = x1.symmetric_difference(x)
+print(r)
+print(type(r))
+print()
+print("---------------------- issubset() ----------------------")
+x = {'a','b','c'}
+x1 = {'f','e','b','d','c'}
+r = x1.issubset(x)
+print(r)
+print(type(r))
+print()
+
+print("---------------------- issuperset() ----------------------")
+
+x = {'a','b','c'}
+x1 = {'f','e','b','d','c'}
+r = x1.issuperset(x)
+print(r)
+print(type(r))
+print()
+
+print("---------------------- frozenset() ----------------------")
+
+l = ['a','b','c']
+f = frozenset(l)
+print(f)
+print(type(f))
+
+# f[1] = '3'
+# TypeError: 'frozenset' object does not support item assignment
 
 
 
---
+++ clg/T2/Code/lamda.py
@@ -0,0 +1,49 @@
+from functools import reduce
+
+a = "mihir"
+print(lambda a : a)
+
+def sq(n):
+    return n*n
+print(sq(4))
+
+print()
+
+s = lambda n:n*n
+print(s(4))
+print(s(5))
+
+print()
+
+Max = lambda a,b : a if(a>b) else b
+print(Max(1,3))
+
+print()
+
+print("--------  lamda with filter() ----------")
+
+li = [5,7,22,97,54,62,77,23,73,61]
+f = list(filter(lambda x:(x % 2 != 0),li))
+print(f)
+
+f = list(filter(lambda x:(x > 18),li))
+print(f)
+
+print("--------  lamda with map() ----------")
+
+li = [5,7,22,97,54,62,77,23,73,61]
+f = list(map(lambda x:x*2,li))
+print(f)
+
+li = ["avcx","gvhbj"]
+f = list(map(lambda x: str.upper(x) , li))
+print(f)
+
+li = [1,2,3,4,5]
+sum = reduce(lambda x,y:x+y , li)
+print(sum)
+print()
+li = [1,2,3,4,5,-1,-7,88]
+sum = reduce(lambda x,y:x if x>y else y, li)
+print(sum)
+
